b44368db88c185897ed543a7bbdd3b210630657c
Yuval Alfassi <yuvalalfassi@gmail.com>
2022-03-20 22:52:22 +0200

    Updated sbatch files

--- sbatch_files/sbatch_random.sh
+++ sbatch_files/sbatch_random.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=yuval_sos_poly
 #SBATCH --mail-user=yuvalalfassi@gmail.com
-#SBATCH --array=0-10
+#SBATCH --array=0-9
 
 module load Python/3.7.4-GCCcore-8.3.0
 module load CMake/3.20.1-GCCcore-10.3.0
@@ -10,6 +10,7 @@ module load CMake/3.20.1-GCCcore-10.3.0
 python3 -m venv ../venv
 source ../venv/bin/activate
 pip3 install --upgrade pip
-pip3 install -r ../requirements.txt
+pip3 install cmake
+pip3 install cvxpy
 
 python3 ../gen_poly_basis.py $(($i)) ../results < input_random.txt
